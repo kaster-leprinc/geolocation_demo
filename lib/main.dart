cbfa51e0ce5b948864291d86a8853f464a3363d5
Nauzet Hernández <nauzet.aduen@gmail.com>
2019-03-07 17:07:40 +0000

    Add multiple containers to check behavior in our listview

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 void main() => runApp(MyApp());
 
@@ -26,40 +29,82 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  String _location = "";
+  // Completer<GoogleMapController> _controller = Completer();
+  GoogleMapController gmController;
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.green,
+        appBar: AppBar(
+          title: Text("AAAA"),
+        ),
+        body: ListView(
           children: <Widget>[
-            Text(
-              'Your Location : ',
-            ),
-            Text(
-              '$_location',
-            ),
+            _buildRedContainer(),
+            SizedBox( height: 20.0),
+            _buildMap(),
+            SizedBox( height: 20.0),
+            _buildRedContainer(),
+            SizedBox( height: 20.0),
+            _buildRedContainer(),
+            SizedBox( height: 20.0),
+            _buildRedContainer(),
+            SizedBox( height: 20.0),
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _getLocation,
-        tooltip: 'Increment',
-        child: Icon(Icons.map),
+        floatingActionButton: FloatingActionButton.extended(
+          onPressed: _goToLocation,
+          label: Text('Our position'),
+          icon: Icon(Icons.directions),
+        ),
       ),
     );
   }
 
-  Future _getLocation() async {
-    print("llamamos a getlocation");
-    Position position = await Geolocator().getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
-    setState(() {
-     _location =position.toString(); 
-    });
+  Widget _buildRedContainer() {
+    return Container(
+      height: 75,
+      width: double.infinity,
+      color: Colors.red,
+      child: Center(child: Text("aquí habría botones")),
+    );
+  }
+
+  Widget _buildMap() {
+    var getInitPos = _getInitPos();
+    return Center(
+      child: Container(
+        height: 300,
+        width: 300,
+        child: GoogleMap(
+          mapType: MapType.terrain,
+          initialCameraPosition: getInitPos,
+          onMapCreated: (controller){
+          setState(() {
+           gmController = controller;
+          //  _goToLocation();
+          });
+        },),
+      ),
+    );
+  }
+
+  CameraPosition _getInitPos() {
+    // Position position = await Geolocator().getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
+    return CameraPosition(
+        target: LatLng(28.099076704839632, -15.419923432062603), zoom: 17.9);
+  }
+
+  Future _goToLocation() async {
+    Position position = await Geolocator()
+        .getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
+    print(position);
+    CameraPosition myLocation = CameraPosition(
+        target: LatLng(position.latitude, position.longitude), zoom: 17.9);
+
+    gmController.animateCamera(CameraUpdate.newCameraPosition(myLocation));
+    //gmController.animateCamera(CameraUpdate.newCameraPosition(CameraPosition(target: LatLng(0,0))));
   }
 }
